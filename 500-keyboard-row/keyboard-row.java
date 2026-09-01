7ded661764235de17afdf47b5dcc97376e1c7fc0
Kushagra Agarwal <kushagra2004.agrwl@gmail.com>
2026-09-01 23:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (51.72%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String row1 = "qwertyuiop";
+        String row2= "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for(String word : words){
+            String w = word.toLowerCase();
+
+            boolean r1 = true;
+            boolean r2 = true;
+            boolean r3 = true;
+
+            for(char ch : w.toCharArray()){
+                if(row1.indexOf(ch)== -1){
+                    r1 = false;
+                }
+                if(row2.indexOf(ch)== -1){
+                    r2 = false;
+                }
+                if(row3.indexOf(ch)== -1){
+                    r3 = false;
+                }
+            }
+            if(r1 || r2 || r3 ){
+                ans.add(word);
+            }
+        }    
+        return ans.toArray(new String[0]);
+    }
+}

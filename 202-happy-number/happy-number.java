1507db7159ca6c91a4879986569f220178e0491e
Kushagra Agarwal <kushagra2004.agrwl@gmail.com>
2026-09-14 09:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (34.02%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+     static int squareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum = sum + (digit * digit);
+            n = n / 10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        
+    while (n != 1 && n != 4) {
+        n = squareSum(n);
+    }
+
+    if (n == 1) {
+        return true;
+    } else {
+        return false;
+    }
+    }
+
+}

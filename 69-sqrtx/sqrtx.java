604cc9456ef5a8c5406b25382f6cafa02bb91648
Kushagra Agarwal <kushagra2004.agrwl@gmail.com>
2026-09-18 21:56:11 +0530

    Time: 1 ms (99.37%) | Memory: 42.7 MB (23.70%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int s=1;
+        int e=x;
+        long ans=0;
+        while(s<=e){
+           int  mid = s+(e-s)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return (int)ans;
+    }
+}

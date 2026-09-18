823b56c181f0d8216acb285ee296d293a5ad06b1
Kushagra Agarwal <kushagra2004.agrwl@gmail.com>
2026-09-18 21:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (91.31%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int beg = 0, end = nums.length - 1, mid;
+        
+
+        while (beg <= end) {
+            mid = (beg + end) / 2;
+            if (nums[mid] == target) {
+               
+                return (mid );
+               
+             
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                beg = mid + 1;
+            }
+        }
+
+          return -1;
+           
+    }
+    
+}
